a04657026a15adae0b3eb08bc6a06304c2294686
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-10-25 08:42:55 +0600

    create API

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
-
 const port = process.env.port || 5000
+
+const categories = require('./data/category.json')
+const courses = require('./data/course.json')
+
+
 
 app.listen(port,() =>{
     console.log('server is running', port)
