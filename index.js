ab5aeb31bc6096bdcefef922c9e78897d1813e4b
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-10-26 11:07:37 +0600

    final commit

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const cors = require('cors')
 app.use(cors())
 const port = process.env.port || 5000
 
-// const categories = require('./data/category.json')
+const categories = require('./data/category.json')
 const courses = require('./data/course.json')
 
 app.get('/', (req, res)=>{
     res.send('Courses API is running')
 })
-
+app.get('/category-course', (req, res)=>{
+    res.send(categories)
+})
 app.get('/course-category', (req, res)=>{
     res.send(courses)
 })
-app.get('/course/:id', (req, res)=>{
+app.get('/courseData/:id', (req, res)=>{
     const id = req.params.id ;
     const selectedId = courses.find(c => c.id === id) 
     res.send(selectedId)
